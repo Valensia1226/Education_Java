9d06d16907eb52ecc53ea972755d4a121d08c834
Darya <39706573+Valensia1226@users.noreply.github.com>
2023-07-30 15:18:06 +0300

    Create HomeWork3.java

---
+++ Exception/HomeWork3.java
@@ -0,0 +1,9 @@
+public class HomeWork3 {
+
+    public static void main(String[] args) {
+        DataByParts dataByParts = new DataByParts();
+        dataByParts.dataToFile();
+        AllDataAtOnce allDataAtOnce = new AllDataAtOnce();
+        //allDataAtOnce.dataToFile();
+    }
+}
